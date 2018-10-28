0185cde578e17a632c0a96d9957afea984acf2ed
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-28 12:54:20 +0100

    add deleting button

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross= document.createElement('div');
 
     li.setAttribute("data-id", doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent= 'X';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
+
+    //Deleting data
+    cross.addEventListener('click', (e) =>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
 
 }
 
