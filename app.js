cbb02cdf0c14280a9fe881a433f9237b01e02f63
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-28 18:03:32 +0100

    real-time data

--- app.js
+++ app.js
@@ -29,20 +29,34 @@ function renderCafe(doc){
 }
 
 //Getting data
-db.collection('cafes').get().then((snapshot) => {
-    snapshot.docs.forEach(element => {
-        renderCafe(element);
-    });
-})
+// db.collection('cafes').orderBy('name').get().then((snapshot) => {
+//     snapshot.docs.forEach(element => {
+//         renderCafe(element);
+//     });
+// })
 
 //Saving data
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    if(form.name.value != '' && form.city.value != ''){
     db.collection('cafes').add({
         name: form.name.value,
         city: form.city.value
     });
     form.name.value = '';
     form.city.value = '';
-    
+}
+});
+
+//real-time listner
+db.collection('cafes').orderBy('name').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change =>{
+        if(change.type == 'added'){
+            renderCafe(change.doc);
+        } else if(change.type == 'removed'){
+            let li = cafeList.querySelector('[data-id=' +change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    })
 })
