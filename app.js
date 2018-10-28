5f009df253382ff49619343e04ad2f6078a1fa6f
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-28 12:43:16 +0100

    add button to add cafes

---
+++ app.js
@@ -0,0 +1,38 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-from');
+
+//Create element and render cafe
+function renderCafe(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute("data-id", doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+
+}
+
+//Getting data
+db.collection('cafes').get().then((snapshot) => {
+    snapshot.docs.forEach(element => {
+        renderCafe(element);
+    });
+})
+
+//Saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+    
+})
